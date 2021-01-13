b9ffa17b3e4e31766fe049928998fb7f1b9db271
ou_glo <ouchanglu_glo@.163>
2021-01-13 17:31:41 +0800

    mybatis - config

---
+++ src/com/atguigu/mybatis/bean/Employee.java
@@ -0,0 +1,44 @@
+package com.atguigu.mybatis.bean;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("emp")
+public class Employee {
+	
+	private Integer id;
+	private String lastName;
+	private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"id=" + id +
+				", lastName='" + lastName + '\'' +
+				", email='" + email + '\'' +
+				'}';
+	}
+}
